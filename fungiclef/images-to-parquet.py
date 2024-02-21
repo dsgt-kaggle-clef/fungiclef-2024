dc22196ccba0d590765399293e1a790cc44aacc7
Maximilian Heil <heil.maximilian95@gmail.com>
2024-02-21 23:06:44 +0000

    adjusted scripts

--- fungiclef/images-to-parquet.py
+++ fungiclef/images-to-parquet.py
@@ -3,7 +3,7 @@ import argparse
 import os
 from pathlib import Path
 
-from pyspark.sql.functions import element_at, regexp_replace, split
+from pyspark.sql.functions import element_at, split
 
 from utils import get_spark
 
@@ -13,67 +13,40 @@ Use the bash file `download_extract_dataset.sh` in the scripts folder.
 """
 
 
-def create_dataframe(spark, base_dir: Path, raw_root_path: str, meta_dataset_name: str):
+def create_dataframe(spark, images_path: Path, raw_root_path: str, meta_data_path: str):
     """Converts images into binary data and joins with a Metadata DataFrame"""
     # Load all image files from the base directory as binary data
     image_df = (
         spark.read.format("binaryFile")
         .option("pathGlobFilter", "*.jpg")
-        .option("recursiveFileLookup", "true")
-        .load(base_dir.as_posix())
+        .load(images_path.as_posix())
     )
-
-    # Construct the string to be replaced - adjust this based on your actual base path
-    to_remove = "file:" + str(base_dir.parents[0])
-
-    # Remove "file:{base_dir.parents[0]" from path column
-    image_df = image_df.withColumn("path", regexp_replace("path", to_remove, ""))
 
     # Split the path into an array of elements
     split_path = split(image_df["path"], "/")
 
     # Extract metadata from the file path
     image_final_df = (
-        image_df.withColumn("folder_name", element_at(split_path, -4))
-        .withColumn("year", element_at(split_path, -3))
-        .withColumn("binomial_name", element_at(split_path, -2))
-        .withColumn("file_name", element_at(split_path, -1))
+        image_df.withColumn("image_path", element_at(split_path, -1))
     )
 
     # Select and rename columns to fit the target schema, including renaming 'content' to 'image_binary_data'
     image_final_df = image_final_df.select(
-        "path",
-        "folder_name",
-        "year",
-        "binomial_name",
-        "file_name",
+        "image_path",
         image_final_df["content"].alias("data"),
     )
 
-    # Create a new column "image_path" by removing "/SnakeCLEF2023-small_size/" from "path"
-    # This column will be used to join with the metadata df later
-    image_final_df = image_final_df.withColumn(
-        "image_path", regexp_replace("path", f"^/{base_dir.parts[-1]}/", "")
-    )
-
-    # Read the iNaturalist metadata CSV file
+    # Read the metadata CSV file & cache file
     meta_df = spark.read.csv(
-        f"{raw_root_path}/{meta_dataset_name}.csv",
+        f"{meta_data_path}.csv",
         header=True,
         inferSchema=True,
     )
 
-    # Cache the DataFrame to optimize subsequent operations
-    meta_df.cache()
-
-    # Drop duplicate entries based on 'image_path' before the join
-    meta_df = meta_df.dropDuplicates(["image_path"])
-
-    # Drop 'binomial_name' column since before joining with image_final_df
-    meta_final_df = meta_df.drop("binomial_name")
+    # No duplicates or columns to drop
 
     # Perform an inner join on the 'image_path' column
-    final_df = image_final_df.join(meta_final_df, "image_path", "inner")
+    final_df = image_final_df.join(meta_df, "image_path", "inner")
 
     return final_df
 
@@ -98,13 +71,13 @@ def parse_args():
     parser.add_argument(
         "--output-path",
         type=str,
-        default="gs://dsgt-clef-fungiclef-2024/data/parquet_files/DF20",
+        default="gs://dsgt-clef-fungiclef-2024/data/parquet/DF20",
         help="GCS path for output Parquet files",
     )
     parser.add_argument(
         "--dataset-name",
         type=str,
-        default="DF20",
+        default="DF20_300",
         help="Dataset name downloaded from tar file",
     )
     parser.add_argument(
@@ -125,14 +98,16 @@ def main():
     spark = get_spark()
 
     # Convert image-root-path to a Path object here
-    base_dir = Path(args.image_root_path) / "data" / args.dataset_name
+    images_path = Path('../../../')  / Path(args.image_root_path) / "data" / args.dataset_name
+
+    meta_data_path = Path('../../../')  / Path(args.image_root_path) / "data" / args.meta_dataset_name
 
     # Create image dataframe
     final_df = create_dataframe(
         spark=spark,
-        base_dir=base_dir,
+        images_path=images_path,
         raw_root_path=args.raw_root_path,
-        meta_dataset_name=args.meta_dataset_name,
+        meta_data_path=meta_data_path,
     )
 
     # Write the DataFrame to GCS in Parquet format
