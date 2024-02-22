dd3306b128540314d8ba698adbd2f83d2b450636
Maximilian Heil <heil.maximilian95@gmail.com>
2024-02-22 16:33:23 +0000

    set up config for all files and to run parqueifying steps

--- fungiclef/images-to-parquet.py
+++ fungiclef/images-to-parquet.py
@@ -1,9 +1,9 @@
 # From SnakeCLEF/Murillo Gustinelli
 import argparse
-import os
+import json
 from pathlib import Path
 
-from pyspark.sql.functions import element_at, split
+from pyspark.sql.functions import element_at, split, regexp_replace
 
 from utils import get_spark
 
@@ -13,9 +13,10 @@ Use the bash file `download_extract_dataset.sh` in the scripts folder.
 """
 
 
-def create_dataframe(spark, images_path: Path, raw_root_path: str, meta_data_path: str):
+def create_dataframe(spark, images_path: Path, metadata_path: str):
     """Converts images into binary data and joins with a Metadata DataFrame"""
     # Load all image files from the base directory as binary data
+
     image_df = (
         spark.read.format("binaryFile")
         .option("pathGlobFilter", "*.jpg")
@@ -38,80 +39,49 @@ def create_dataframe(spark, images_path: Path, raw_root_path: str, meta_data_pat
 
     # Read the metadata CSV file & cache file
     meta_df = spark.read.csv(
-        f"{meta_data_path}.csv",
+        f"{metadata_path}",
         header=True,
         inferSchema=True,
     )
 
-    # No duplicates or columns to drop
+    # replace .JPG to .jpg
+    meta_df = (
+        meta_df.withColumn("image_path", regexp_replace('image_path', ".JPG", '.jpg'))
+    )
 
     # Perform an inner join on the 'image_path' column
-    final_df = image_final_df.join(meta_df, "image_path", "inner")
+    final_df = image_final_df.join(meta_df, "image_path", "left")
 
     return final_df
 
 
-def parse_args():
-    """Parse command line arguments."""
-    parser = argparse.ArgumentParser(
-        description="Process images and metadata for a dataset stored on GCS."
-    )
-    parser.add_argument(
-        "--image-root-path",
-        type=str,
-        default=str(Path(".").anchor / Path("mnt/")),
-        help="Base directory path for image data",
-    )
-    parser.add_argument(
-        "--raw-root-path",
-        type=str,
-        default="gs://dsgt-clef-fungiclef-2024/raw/",
-        help="Root directory path for metadata",
-    )
-    parser.add_argument(
-        "--output-path",
-        type=str,
-        default="gs://dsgt-clef-fungiclef-2024/data/parquet/DF20",
-        help="GCS path for output Parquet files",
-    )
-    parser.add_argument(
-        "--dataset-name",
-        type=str,
-        default="DF20_300",
-        help="Dataset name downloaded from tar file",
-    )
-    parser.add_argument(
-        "--meta-dataset-name",
-        type=str,
-        default="FungiCLEF2023_train_metadata_PRODUCTION",
-        help="Train Metadata CSV file",
-    )
-
-    return parser.parse_args()
+def read_config():
+    with open('fungiclef/config.json') as f:
+        config = json.load(f)
+    return config
 
 
 def main():
     """Main function that processes data and writes the output dataframe to GCS"""
-    args = parse_args()
+    config = read_config()
 
     # Initialize Spark
     spark = get_spark()
 
-    # Convert image-root-path to a Path object here
-    images_path = Path('../../../')  / Path(args.image_root_path) / "data" / args.dataset_name
-
-    meta_data_path = Path('../../../')  / Path(args.image_root_path) / "data" / args.meta_dataset_name
+    # Set Paths - adjust as needed
+    images_path = Path('../../../')  / Path(config["mnt_data_paths"]) / Path("DF20")
+    metadata_path = config["gs_paths"]["raw"]["training_metadata"]
+    outpout_path = config["gs_paths"]["parquet"]["training_data"] # here no path
 
     # Create image dataframe
     final_df = create_dataframe(
         spark=spark,
         images_path=images_path,
-        raw_root_path=args.raw_root_path,
-        meta_data_path=meta_data_path,
+        metadata_path=metadata_path,
     )
 
     # Write the DataFrame to GCS in Parquet format
-    final_df.write.mode("overwrite").parquet(args.output_path)
+    final_df.write.mode("overwrite").parquet(outpout_path)
 
 
 if __name__ == "__main__":
