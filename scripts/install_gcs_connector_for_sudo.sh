ae2c1ab93ac503475d78b1ea1405ff0823e18dcf
Maximilian Heil <heil.maximilian95@gmail.com>
2024-04-19 22:45:50 +0000

    almost completed clip embeding code & test

--- scripts/install_gcs_connector_for_sudo.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-
-lib_dir="home/fungi/lib/python3.10/site-packages/pyspark/jars"
-mkdir -p "${lib_dir}"
-cd "${lib_dir}"
-echo "Installing to $lib_dir."
-
-wget https://storage.googleapis.com/hadoop-lib/gcs/gcs-connector-hadoop3-latest.jar
