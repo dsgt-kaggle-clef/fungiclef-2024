4a88b9a3b499fd4a3f025991fd2f39621cc92417
Maximilian Heil <heil.maximilian95@gmail.com>
2024-04-21 17:43:01 +0000

    Implemented CLIP and clip workflows

--- run_embeddings_wf.py
+++ run_embeddings_wf.py
@@ -11,13 +11,15 @@ parser.add_argument(
 parser.add_argument(
     "--train-data-path",
     type=str,
-    default="data/parquet/DF20_300px_and_DF21_300px_train",
+    default="dev/dev_train",
+    #default="data/parquet/DF20_300px_and_DF21_300px_train",
     help="Root directory for training data in GCS",
 )
 parser.add_argument(
     "--output-name-path",
     type=str,
-    default="data/parquet/DF20_300px_and_DF21_300px_train_embedding",
+    default="dev/dev_embedded",
+    #default="data/parquet/DF20_300px_and_DF21_300px_train_embedding",
     help="GCS path for output Parquet files",
 )
 
@@ -27,4 +29,4 @@ args = parser.parse_args()
 input_path = f"{args.gcs_root_path}/{args.train_data_path}"
 output_path = f"{args.gcs_root_path}/{args.output_name_path}"
 
-run_embeddings_wf(input_path=input_path, output_path=output_path)
+run_embeddings_wf(input_path=input_path, output_path=output_path, num_samples=0)
