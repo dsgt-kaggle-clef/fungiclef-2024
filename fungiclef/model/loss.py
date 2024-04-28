c5fa28ffe422d9e92bea3320ec5490a2bfe2f0c2
Chris <chy.chiu@gmail.com>
2024-04-28 10:08:28 +0000

    wrapper

--- fungiclef/model/loss.py
+++ fungiclef/model/loss.py
@@ -2,16 +2,130 @@ from typing import Literal
 from kornia.losses import focal_loss
 from torch import nn
 
-class FungiModelLoss():
+import numpy as np
+import torch
+import torch.nn as nn
+from typing import Union
 
-    def __init__(self, loss: Literal['cross_entropy', 'focal_loss']):
+
+## From https://github.com/bamps53/SeesawLoss/blob/master/seesaw_loss.py
+
+
+class SeesawLossWithLogits(nn.Module):
+    """
+    This is unofficial implementation for Seesaw loss,
+    which is proposed in the techinical report for LVIS workshop at ECCV 2020.
+    For more detail, please refer https://arxiv.org/pdf/2008.10032.pdf.
+
+    Args:
+    class_counts: The list which has number of samples for each class.
+                  Should have same length as num_classes.
+    p: Scale parameter which adjust the strength of panishment.
+       Set to 0.8 as a default by following the original paper.
+    """
+
+    def __init__(self, class_counts: Union[list, np.array], p: float = 0.8):
+        super().__init__()
+
+        class_counts = torch.FloatTensor(class_counts)
+        conditions = class_counts[:, None] > class_counts[None, :]
+        trues = (class_counts[None, :] / class_counts[:, None]) ** p
+        falses = torch.ones(len(class_counts), len(class_counts))
+        self.s = torch.where(conditions, trues, falses)
+
+        self.num_classes = len(class_counts)
+
+        self.eps = 1.0e-6
+
+    def forward(self, logits, targets):
+        self.s = self.s.to(targets.device)
+        max_element, _ = logits.max(axis=-1)
+        logits = logits - max_element[:, None]  # to prevent overflow
+
+        numerator = torch.exp(logits)
+
+        if len(targets.shape) == 1:
+            _targets = (
+                torch.zeros(targets.shape[0], self.num_classes).int().to(targets.device)
+            )
+            _targets[:, targets] = 1
+            targets = _targets
+
+        denominator = (
+            (1 - targets)[:, None, :]
+            * self.s[None, :, :]
+            * torch.exp(logits)[:, None, :]
+        ).sum(axis=-1) + torch.exp(logits)
+
+        sigma = numerator / (denominator + self.eps)
+        loss = (-targets * torch.log(sigma + self.eps)).sum(-1)
+        return loss.mean()
+
+
+class DistibutionAgnosticSeesawLossWithLogits(nn.Module):
+    """
+    This is unofficial implementation for Seesaw loss,
+    which is proposed in the techinical report for LVIS workshop at ECCV 2020.
+    For more detail, please refer https://arxiv.org/pdf/2008.10032.pdf.
+
+    Args:
+    p: Scale parameter which adjust the strength of panishment.
+       Set to 0.8 for default following the paper.
+    """
+
+    def __init__(self, p: float = 0.8):
+        super().__init__()
+        self.eps = 1.0e-6
+        self.p = p
+        self.s = None
+        self.class_counts = None
+
+    def forward(self, logits, targets):
+        if self.class_counts is None:
+            self.class_counts = targets.sum(axis=0) + 1  # to prevent devided by zero.
+        else:
+            self.class_counts += targets.sum(axis=0)
+
+        conditions = self.class_counts[:, None] > self.class_counts[None, :]
+        trues = (self.class_counts[None, :] / self.class_counts[:, None]) ** self.p
+        falses = torch.ones(len(self.class_counts), len(self.class_counts))
+        self.s = torch.where(conditions, trues, falses)
+
+        max_element, _ = logits.max(axis=-1)
+        logits = logits - max_element[:, None]  # to prevent overflow
+
+        numerator = torch.exp(logits)
+        denominator = (
+            (1 - targets)[:, None, :]
+            * self.s[None, :, :]
+            * torch.exp(logits)[:, None, :]
+        ).sum(axis=-1) + torch.exp(logits)
+
+        sigma = numerator / (denominator + self.eps)
+        loss = (-targets * torch.log(sigma + self.eps)).sum(-1)
+        return loss.mean()
+
+
+class FungiModelLoss:
+
+    def __init__(
+        self,
+        loss: Literal["cross_entropy", "focal_loss", "seesaw"],
+        class_distribution=None,
+    ):
+
+        if loss == "seesaw":
+            if class_distribution:
+                self.seesaw = SeesawLossWithLogits(class_distribution)
+            else:
+                self.seesaw = DistibutionAgnosticSeesawLossWithLogits()
 
         self.loss_fn = loss
 
     def loss(self, pred, target):
-        if self.loss_fn == 'cross_entropy':
+        if self.loss_fn == "cross_entropy":
             return nn.functional.cross_entropy(pred, target)
         elif self.loss_fn == "focal_loss":
             return focal_loss(pred, target, alpha=0.25, reduction="mean")
-        
-        
+        elif self.loss_fn == "seesaw":
+            return self.seesaw(pred, target)
--- fungiclef/model/wrapper.py
+++ fungiclef/model/wrapper.py
@@ -13,60 +13,99 @@ N_CLASSES = 1605
 # define the LightningModule
 class FungiModel(L.LightningModule):
     # Simple wrapper for a torch nn.Module, with specified loss / optimizer.
-    # This is supposed to be model agnostic. 
+    # This is supposed to be model agnostic.
 
     def __init__(self, model: nn.Module, optimizer=None, loss=None):
         super().__init__()
         self.model = model
-        self.optimizer = optimizer if optimizer else optim.Adam(self.parameters(), lr=1e-3)
+        self.optimizer = (
+            optimizer if optimizer else optim.Adam(self.parameters(), lr=1e-5)
+        )
 
         self.poison_mapping = get_poison_mapping().to(self.device)
 
         self.loss = loss if loss else nn.functional.cross_entropy
         self.train_loss = []
-        self.train_accuracy_class_top_1 = Accuracy(task="multiclass", num_classes=N_CLASSES)
-        self.valid_accuracy_class_top_1 = Accuracy(task="multiclass", num_classes=N_CLASSES)
-        self.train_accuracy_class_top_3 = Accuracy(task="multiclass", num_classes=N_CLASSES, top_k=3)
-        self.valid_accuracy_class_top_3 = Accuracy(task="multiclass", num_classes=N_CLASSES, top_k=3)
+        self.train_accuracy_class_top_1 = Accuracy(
+            task="multiclass", num_classes=N_CLASSES
+        )
+        self.valid_accuracy_class_top_1 = Accuracy(
+            task="multiclass", num_classes=N_CLASSES
+        )
+        self.train_accuracy_class_top_3 = Accuracy(
+            task="multiclass", num_classes=N_CLASSES, top_k=3
+        )
+        self.valid_accuracy_class_top_3 = Accuracy(
+            task="multiclass", num_classes=N_CLASSES, top_k=3
+        )
         self.train_accuracy_poison = Accuracy(task="binary")
-        self.valid_accuracy_poison =  Accuracy(task="binary")
-        
+        self.valid_accuracy_poison = Accuracy(task="binary")
+
     def training_step(self, batch, batch_idx):
-        
+
         x, y = batch
         y_pred = self.model(x)
         loss = self.loss(y_pred, y)
         self.train_loss.append(loss)
-        
-        poison_prediction = self.poison_mapping[y_pred.argmax(1).to(self.poison_mapping.device)]
+
+        poison_prediction = self.poison_mapping[
+            y_pred.argmax(1).to(self.poison_mapping.device)
+        ]
         poison_gt = self.poison_mapping[y.int().to(self.poison_mapping.device)]
-        
+
         self.train_accuracy_class_top_1.update(y_pred, y)
         self.train_accuracy_class_top_3.update(y_pred, y)
         self.train_accuracy_poison.update(poison_prediction, poison_gt)
 
         return loss
-    
+
     def on_train_epoch_end(self):
         all_train_loss = torch.stack(self.train_loss)
         # do something with all preds
-        print(all_train_loss.shape)
         avg_train_loss = all_train_loss.mean()
-        self.log("train_loss", avg_train_loss, prog_bar=True, logger=True, sync_dist=True, on_epoch=True)
-        
+        self.log(
+            "train_loss",
+            avg_train_loss,
+            prog_bar=True,
+            logger=True,
+            sync_dist=True,
+            on_epoch=True,
+        )
+
         self.train_accuracy_class_top_1.compute()
         self.train_accuracy_class_top_3.compute()
         self.train_accuracy_poison.compute()
 
-        self.log("train_acc_cls", self.train_accuracy_class_top_1.compute(), prog_bar=True, logger=True, sync_dist=True, on_epoch=True)
-        self.log("train_acc_cls_top_3", self.train_accuracy_class_top_3.compute(), prog_bar=True, logger=True, sync_dist=True, on_epoch=True)
-        self.log("train_acc_poison", self.train_accuracy_poison.compute(), prog_bar=True, logger=True, sync_dist=True, on_epoch=True)
-                
+        self.log(
+            "train_acc_cls",
+            self.train_accuracy_class_top_1.compute(),
+            prog_bar=True,
+            logger=True,
+            sync_dist=True,
+            on_epoch=True,
+        )
+        self.log(
+            "train_acc_cls_top_3",
+            self.train_accuracy_class_top_3.compute(),
+            prog_bar=True,
+            logger=True,
+            sync_dist=True,
+            on_epoch=True,
+        )
+        self.log(
+            "train_acc_poison",
+            self.train_accuracy_poison.compute(),
+            prog_bar=True,
+            logger=True,
+            sync_dist=True,
+            on_epoch=True,
+        )
+
         self.train_accuracy_class_top_1.reset()
         self.train_accuracy_class_top_3.reset()
         self.train_accuracy_poison.reset()
         self.train_loss.clear()  # free memory
-    
+
     def validation_step(self, batch, batch_idx):
         # this is the validation loop
         x, y = batch
@@ -74,11 +113,15 @@ class FungiModel(L.LightningModule):
         y_pred = self.model(x)
         loss = self.loss(y_pred, y)
 
-        self.log("val_loss", loss, prog_bar=True, logger=True, sync_dist=True, on_epoch=True)
+        self.log(
+            "val_loss", loss, prog_bar=True, logger=True, sync_dist=True, on_epoch=True
+        )
 
-        poison_prediction = self.poison_mapping[y_pred.argmax(1).to(self.poison_mapping.device)]
+        poison_prediction = self.poison_mapping[
+            y_pred.argmax(1).to(self.poison_mapping.device)
+        ]
         poison_gt = self.poison_mapping[y.int().to(self.poison_mapping.device)]
-        
+
         self.valid_accuracy_class_top_1.update(y_pred, y)
         self.valid_accuracy_class_top_3.update(y_pred, y)
         self.valid_accuracy_poison.update(poison_prediction, poison_gt)
@@ -87,10 +130,31 @@ class FungiModel(L.LightningModule):
         self.valid_accuracy_class_top_3.compute()
         self.valid_accuracy_poison.compute()
 
-        self.log("valid_acc_cls", self.valid_accuracy_class_top_1.compute(), prog_bar=True, logger=True, sync_dist=True, on_epoch=True)
-        self.log("valid_acc_cls_top_3", self.valid_accuracy_class_top_3.compute(), prog_bar=True, logger=True, sync_dist=True, on_epoch=True)
-        self.log("valid_acc_poison", self.valid_accuracy_poison.compute(), prog_bar=True, logger=True, sync_dist=True, on_epoch=True)
-                
+        self.log(
+            "valid_acc_cls",
+            self.valid_accuracy_class_top_1.compute(),
+            prog_bar=True,
+            logger=True,
+            sync_dist=True,
+            on_epoch=True,
+        )
+        self.log(
+            "valid_acc_cls_top_3",
+            self.valid_accuracy_class_top_3.compute(),
+            prog_bar=True,
+            logger=True,
+            sync_dist=True,
+            on_epoch=True,
+        )
+        self.log(
+            "valid_acc_poison",
+            self.valid_accuracy_poison.compute(),
+            prog_bar=True,
+            logger=True,
+            sync_dist=True,
+            on_epoch=True,
+        )
+
         self.valid_accuracy_class_top_1.reset()
         self.valid_accuracy_class_top_3.reset()
         self.valid_accuracy_poison.reset()
